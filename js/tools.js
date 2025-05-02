c121b40746d4d62ffb08715a26c4eea11fc855c7
loictonneau75 <loictonneau75@gmail.com>
2025-05-02 12:31:04 +0200

    completion de la fonction createcustom element et  creation de cureaterowwithcolum

--- js/tools.js
+++ js/tools.js
@@ -5,7 +5,7 @@ export async function getConfigValue(key) {
         return data[key];
     } catch(error){
         console.error("Erreur lors de la récupération du JSON : ", error);
-        return null
+        return null;
     }
 };
 
@@ -16,12 +16,41 @@ function requiredParam(paramName){
 export function createCustomElement({
     tag = requiredParam("name"), 
     innerText = null,
-    classList = null}){
-    const element = document.createElement(tag)
+    classList = null,
+    id = null,
+    autocomplete = null,
+    htmlFor = null,
+    type = null,
+    placeholder = null,
+    required = null
+}){
+    const element = document.createElement(tag);
     
-    if(innerText){element.innerText = innerText}
+    if(innerText){element.innerText = innerText};
     
     element.classList.add(...(classList || []));
 
+    if(id){element.id = id};
+
+    if(autocomplete){element.autocomplete = autocomplete};
+
+    if(htmlFor){element.htmlFor = htmlFor};
+
+    if(type){element.type = type};
+
+    if(placeholder){element.placeholder = placeholder};
+
+    if(required){element.required = required}
+
     return element;
+};
+
+export function createRowWithColumns(contents){
+    const row = createCustomElement({tag: "div",classList: ["row", "g-3"]});
+    contents.forEach(content => {
+        const col = createCustomElement({tag: "div", classList: `col-md-${Math.floor(12 / contents.length)}`});
+        col.appendchild(content);
+        row.appendChild(col);
+    });
+    return row;
 };
